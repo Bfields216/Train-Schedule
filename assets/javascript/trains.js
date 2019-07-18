60db3780a83df9564d2387e53ba1fdc9606f1e59
Brandon Fields <brandonfields10@icloud.com>
2019-07-17 20:39:27 -0400

    got the schedule to load just need to reorganize, its showing multiple schedules at once before anything is submitted

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -21,8 +21,24 @@ console.log("Working Javascript for Train Schedule")
 
     var dataref = firebase.database();
 
+    var currentTime = moment().format();
 
-    
+    //display current time
+    var uclock = function(){
+        clock = $("#currentTime");
+        date = moment(new Date()).format('dddd, MMMM Do YYYY, h:mm:ss a');
+        clock.html('<h3>' + date + '</h3>');
+        //isolates the seconds
+        var indCol = date.indexOf(':');
+        var seconds = (date.substring(indCol + 4, date.length -3));
+
+        // if(seconds == '00'){
+        //     updateTable();
+        // };
+    };
+
+    uclock();
+    setInterval(uclock, 1000);
 
     // on click function for the button
     $("#add-user").on("click", function (event) {
@@ -40,7 +56,13 @@ console.log("Working Javascript for Train Schedule")
         console.log(firstTrain);
         console.log(frequency);
 
-
+        // var newTrain = {
+        //     name: name,
+        //     destination: destination,
+        //     firstTrain: firstTrain,
+        //     frequency: frequency,
+        //     dataAdded: firebase.database.ServerValue.TIMESTAMP
+        // }
 
         // clear input fields after submit
         $("#name").val("");
@@ -58,12 +80,37 @@ console.log("Working Javascript for Train Schedule")
         });
 
     })
-// database.ref().on("child_added", function(snapshot) {
-//     console.log(snapshot.val());
+    //firebase event for when the user adds their train information
+    dataref.ref().on("child_added", function(snapshot) {
+    event.preventDefault();
+        console.log(snapshot.val());
 
-//     name = snapshot.val().name;
-//     destination = snapshot.val().destination;
-// })
+
+    // creating variables to store into firebase
+    var tname = snapshot.val().name;
+    var tdestination = snapshot.val().destination;
+    var tfirstTrain = snapshot.val().firstTrain;
+    var tfrequency = snapshot.val().frequency;
+    //variable to figure out the trains converted time
+    var trainTimeConverted = moment(tfirstTrain, 'HH:mm');
+    //variable to figure out the difference 
+    var timeDifference = moment().diff(moment(trainTimeConverted), "minutes");
+        console.log(timeDifference);
+    
+    var frequencyMinutes = snapshot.val().frequency;
+        console.log("Frequency Minutes: " + frequencyMinutes);
+
+    var minutesAway = Math.abs(timeDifference % frequencyMinutes);
+        console.log("Minutes Away: " + minutesAway);
+
+    var nextArrival = moment(currentTime).add(minutesAway, "minutes").format("hh:mm A");
+        console.log("Next Arrival: " + nextArrival);
+    
+    
+    //Adding into the table
+  $("#schedule-panel").append("<tr><td>" + tname + "</td><td>" + tdestination + "</td><td>" + tfrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+});
+
 
 
 
